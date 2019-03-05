570481e72ed3812d72b367be21b5ad6e9262db52
GuillermoCortes <42586963+GuillermoCortes@users.noreply.github.com>
2019-03-05 12:27:34 -0500

    Update calc_mod.pyx

--- cython/calc_mod.pyx
+++ cython/calc_mod.pyx
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-imc=plt.imread("Bolas2.tif")
+imc=plt.imread("b9.jpeg")
 img=((imc[:,:,0]*0.2989) + (imc[:,:,1]*0.5870) + (imc[:,:,2]*0.1140))/3
 a = (sum(sum(img)))/(10**6)
 l=0
+print a
+print np.size(imc)
+print np.size(img)
 if(a>20):
 	l=60
 	for i in range(len(img)):
@@ -21,6 +25,23 @@ if(a<20):
 			if img[i][j]>l:
 				img[i][j]=1
 
+#En el plt.imshow() salen cuadros amarillos. Donde es amarillo es que vale 1, de no ser asi vale 0.
+k=1
+c1=0
+no=[[],[]]
+k=0
+while(k==0):
+	for i in range(len(img)):
+		for j in range(len(img[i])):
+			if img[i][j]==1:
+				a = img[i][j]
+				no[i][j] = a			
+			if(c1==0):
+				k=1
+			else:
+				k=0
+			c1=0
 plt.imshow(img)
 plt.show()
-#Hasta aqui sería pasarlo con los bordes, ahora toca es calcular los centros de masa y los tiempos.
+plt.imshow(no) #Hay un error aqui
+plt.show()
