35618b55e740cf1068c3d4039c6dab93224881f7
GuillermoCortes <42586963+GuillermoCortes@users.noreply.github.com>
2019-03-06 15:51:11 -0500

    Update calc_mod.pyx

--- cython/calc_mod.pyx
+++ cython/calc_mod.pyx
@@ -12,7 +12,49 @@ def inicio(x):
 	x = x[n:]
 	x = sincer(x)
 	return x 
-
+	
+def contador(x):
+	n=0
+	nc = 0
+	i=0
+	u=0
+	while(x[i]!=0):
+		n=n+1 #Numero de elementos de la primera esfera encontrada en x
+		i=i+1 #Numero del elemento de x hasta que llego.
+	while(x[i]==0):
+		i=i+1
+		nc=nc+1
+	x=x[(n+nc):]
+	u = int(n/2)	#Fila central de las n-esimas filas de x
+	return x, n, nc, u #x: nueva lista. nc= No. de ceros u = 
+		
+def contadorb(x):
+	i=0
+	suma=0
+	bloq=0
+	b=0
+	while(i<len(x)):
+		suma = x[i] + suma
+		if(x[i]!=0):
+			if bloq==0:
+				b = b+1
+				bloq=1
+		if suma==0:
+			bloq=0	
+		suma=0
+		i=i+1		
+	return b
+			
+def centros(x,b)
+	i=0
+	j=0
+	a = []
+	xtemp = []
+	while(i<b):
+		
+		
+	return a
+	
 def sincer(x):
 	i=0
 	n=0
@@ -22,28 +64,7 @@ def sincer(x):
 	x = x[n:]
 	return x
 	
-def concer(x):
-	i=0
-	n=0
-	while(x[i]!=0):
-		n=n+1
-		i=i+1
-	x=x[n:]
-	return x
-def listatemp(x,m):
-	i=m
-	n=0
-	xtemp=[]
-	while(x[i]!=0):
-		xtemp.append(x[i])
-		n=n+1
-		i=i+1
-	return xtemp, n
 
-def centro(x):
-	i=0
-	
-	
 def cont1(img):
 	g=1
 	x=[]
@@ -60,8 +81,12 @@ def cont1(img):
 		g=0
 	return x
 
-
-
+def final(x):
+	xtemp = x[::-1]
+	xtemp = sincer(xtemp)
+	xtemp = xtemp[::-1]
+	return xtemp
+	
 def calc(imagen,dx,dt):
 	imc=plt.imread(imagen)
 	img=((imc[:,:,0]*0.2989) + (imc[:,:,1]*0.5870) + (imc[:,:,2]*0.1140))/3
@@ -83,16 +108,17 @@ def calc(imagen,dx,dt):
 				if img[i][j]>k:
 					img[i][j]=1
 	x=cont1(img) #Contar cuantos "1" hay en cada fila de la matriz de img
-	x=np.array(x) #Todo pasa a ser una lista.
-	x = inicio(x)
-	x1=[]
-	x2=[]
-	x1,n1 = listatemp(x,0)
-	x = inicio(x)
-	x = concer(x)
+	x=np.array(x) #Todo pasa a ser un arreglo.
+	x = inicio(x) #Quitamos el primer conjunto y los primeros ceros.
+	b = contadorb(x)
 	print x
-	print x1
-	print (n1-1)/2 #Hasta aqui tocaria es encontrar los "centros" de cada "bloque" de x.
+	print b
+	plt.imshow(img)
+	plt.show()
+
+
+
 	
 
-calc("Bolas2.tif",0.5,0.1)
+calc("b5.jpeg",0.5,0.1)
+
